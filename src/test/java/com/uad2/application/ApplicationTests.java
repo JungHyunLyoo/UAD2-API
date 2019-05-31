5682899fba87d89a8a4e3459d147a37abaa592de
Charles_Lyoo <dkcmsadkcmsa@gmail.com>
2019-06-01 00:33:27 +0900

    [config]-junit create

--- src/test/java/com/uad2/application/ApplicationTests.java
+++ src/test/java/com/uad2/application/ApplicationTests.java
@@ -1,6 +1,8 @@
 package com.uad2.application;
 
 import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,9 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
-
     @Test
     public void contextLoads() {
+        assertThat(10, is(10));
     }
-
 }
