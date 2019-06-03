e98d0b782f28f344ee0e08d2f180eb985be454d2
Charles_Lyoo <dkcmsadkcmsa@gmail.com>
2019-06-03 20:41:22 +0900

    [log]-logback config

--- src/main/java/com/uad2/application/TestClass.java
+++ src/main/java/com/uad2/application/TestClass.java
@@ -2,6 +2,8 @@ package com.uad2.application;
 
 import com.uad2.application.domain.Member;
 import com.uad2.application.repository.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +12,15 @@ import java.util.List;
 
 @RestController
 public class TestClass {
+    static final Logger logger = LoggerFactory.getLogger(TestClass.class);
     @Autowired
     private MemberRepository memberRepository;
     @GetMapping("/test")
     public String test(){
         List<Member> memberList = memberRepository.findAll();
-        System.out.println(memberList.size());
+        logger.info("info ID : {}", "foo");
+        logger.debug("debug ID : {}", "foo");
+        System.out.println("!23");
         return "test";
     }
 }
