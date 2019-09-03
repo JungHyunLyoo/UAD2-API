20a334eb7bd9f5bdf61aa56908017cfdf1e6b47b
Charles_Lyoo <dkcmsadkcmsa@gmail.com>
2019-09-04 01:23:23 +0900

    [jasypt]-jasypt config class 생성

---
+++ src/main/java/com/uad2/application/config/JasyptConfig.java
@@ -0,0 +1,28 @@
+package com.uad2.application.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        String password = System.getProperty("jasypt.encryptor.password");
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password); //암호화 키
+        config.setAlgorithm("PBEWithMD5AndDES"); //알고리즘
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
