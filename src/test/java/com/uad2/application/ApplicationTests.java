d3626b4172a56907a5644cf774c8dbd5a7dc6afa
Charles_Lyoo <dkcmsadkcmsa@gmail.com>
2019-09-06 00:28:54 +0900

    [test]-application test 클래스 수정

--- src/test/java/com/uad2/application/ApplicationTests.java
+++ src/test/java/com/uad2/application/ApplicationTests.java
@@ -4,14 +4,28 @@ import org.junit.Test;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ApplicationTests {
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
     public void contextLoads() {
         assertThat(10, is(10));
+    /*
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/AP==")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    * */
     }
 }
