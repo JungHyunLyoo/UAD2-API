02468fb4025e090cf137eea95504e36e414ea086
Charles_Lyoo <dkcmsadkcmsa@gmail.com>
2019-09-06 00:24:04 +0900

    [Application]-CommandLineRunner 추가 및 run 메소드 추가

--- src/main/java/com/uad2/application/Application.java
+++ src/main/java/com/uad2/application/Application.java
@@ -1,12 +1,17 @@
 package com.uad2.application;
 
+import com.uad2.application.utils.JasyptEncryptorUtil;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Application.class);
         springApplication.run(args);
+    }
+    @Override
+    public void run(String... args) throws Exception {
     }
 }
