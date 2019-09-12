b453792170eb9dbe8a9690e1f877938ace80e35d
Charles_Lyoo <dkcmsadkcmsa@gmail.com>
2019-09-12 22:17:02 +0900

    [member]-회원 response 데이터 템플릿 생성

---
+++ src/main/java/com/uad2/application/member/resource/MemberExternalResource.java
@@ -0,0 +1,20 @@
+package com.uad2.application.member.resource;
+/*
+ * @USER JungHyun
+ * @DATE 2019-09-12
+ */
+
+import com.uad2.application.member.entity.MemberExternalDto;
+import org.springframework.hateoas.ResourceSupport;
+
+public class MemberExternalResource extends ResourceSupport {//.add로 링크추가 가능
+    private MemberExternalDto memberExternalDto;
+
+    public MemberExternalResource(MemberExternalDto memberExternalDto){
+        this.memberExternalDto = memberExternalDto;
+    }
+
+    public MemberExternalDto getMember() {
+        return memberExternalDto;
+    }//return되는 필드명
+}
---
+++ src/main/java/com/uad2/application/member/resource/MemberListExternalResource.java
@@ -0,0 +1,23 @@
+package com.uad2.application.member.resource;
+/*
+ * @USER JungHyun
+ * @DATE 2019-09-12
+ */
+
+import com.uad2.application.member.entity.Member;
+import com.uad2.application.member.entity.MemberExternalDto;
+import org.springframework.hateoas.ResourceSupport;
+
+import java.util.List;
+
+public class MemberListExternalResource extends ResourceSupport {//.add로 링크추가 가능
+    private List<MemberExternalDto> memberExternalDtoList;
+
+    public MemberListExternalResource(List<MemberExternalDto> memberExternalDtoList){
+        this.memberExternalDtoList = memberExternalDtoList;
+    }
+
+    public List<MemberExternalDto> getMemberList() {
+        return memberExternalDtoList;
+    }
+}
