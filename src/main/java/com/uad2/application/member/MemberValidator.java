acf184c7aa2510102f8664689ebb2e86661f87a3
Charles_Lyoo <dkcmsadkcmsa@gmail.com>
2019-09-29 11:46:22 +0900

    [member]-validator 수정

--- src/main/java/com/uad2/application/member/MemberValidator.java
+++ src/main/java/com/uad2/application/member/MemberValidator.java
@@ -8,12 +8,22 @@ import com.uad2.application.exception.ClientException;
 import com.uad2.application.member.dto.MemberDto;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Field;
+
 @Component
 public class MemberValidator {
-    public void validateCreateMember(MemberDto.Request requestMember) throws ClientException {
-        String id = requestMember.getId();
-        if(id == null || id.isEmpty()){
-            throw new ClientException("id fail");
+    public void validateCreateMember(MemberDto.Request requestMember) {
+        for (Field declaredField : requestMember.getClass().getDeclaredFields()) {
+            //private 필드에 접근 허용
+            declaredField.setAccessible(true);
+            try {
+                Object value = declaredField.get(requestMember);
+                if(value == null || value.toString().isEmpty()){
+                    throw new ClientException(declaredField.getName() + " is empty");
+                }
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException("validateCreateMember error");
+            }
         }
     }
 }
