940916a9dea440e2d1d574f0b20b1b1f5bc68be1
Charles_Lyoo <dkcmsadkcmsa@gmail.com>
2019-10-06 15:38:06 +0900

    [attendance]-get attendance list api

--- src/main/java/com/uad2/application/attendance/repository/AttendanceRepository.java
+++ src/main/java/com/uad2/application/attendance/repository/AttendanceRepository.java
@@ -2,5 +2,11 @@ package com.uad2.application.attendance.repository;
 
 import com.uad2.application.attendance.entity.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface AttendanceRepository extends JpaRepository<Attendance,Long> {}
+import java.util.List;
+
+public interface AttendanceRepository extends JpaRepository<Attendance,Long> {
+    @Query(value="SELECT * FROM Attendance WHERE member_seq = ?1 and available_date like ?2%",nativeQuery = true)
+    List<Attendance> findByMemberSeqAndAvailableDate(int memberSeq, String availableDate);
+}
