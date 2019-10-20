f04eedfef68cd041b5a26c1c9b96c16f0a5ff4f3
박준 호 <pjh6274@gmail.com>
2019-10-21 00:47:14 +0900

    [matching] 매칭 생성 작업중

--- src/main/java/com/uad2/application/attendance/service/AttendanceService.java
+++ src/main/java/com/uad2/application/attendance/service/AttendanceService.java
@@ -10,6 +10,7 @@ import com.uad2.application.attendance.repository.AttendanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -19,5 +20,9 @@ public class AttendanceService {
 
     public List<Attendance> getAttendanceList(int memberSeq,String date){
         return attendanceRepository.findByMemberSeqAndAvailableDate(memberSeq,date);
+    }
+
+    public List<Attendance> getAttendanceAndMemberListByDateAndTime(Date date, String time) {
+        return attendanceRepository.findByAvailableDateAndTime(date, time);
     }
 }
