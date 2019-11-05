6202a0faf8b26155d75bdda9ca53a48b81163fba
Charles_Lyoo <dkcmsadkcmsa@gmail.com>
2019-11-05 23:32:22 +0900

    [calculation]-add test cases

--- src/test/java/com/uad2/application/calculation/controller/CalculationControllerTests.java
+++ src/test/java/com/uad2/application/calculation/controller/CalculationControllerTests.java
@@ -102,22 +102,75 @@ public class CalculationControllerTests  extends BaseControllerTest {
         result.andExpect(status().isOk())
                 .andDo(print());
     }
-/*
+
     @Test
     @Transactional
     @TestDescription("전체 정산 데이터 내역 조회 에러(로그인 x)")
     public void getAllCalculations_badRequest_noAuth() throws Exception {
-    }
 
+        // request
+        ResultActions result = mockMvc.perform(
+                RestDocumentationRequestBuilders.get("/api/calculation/year/{year}/month/{month}/memberSeq/{memberSeq}",2019,10,135)
+        );
+        // result
+        result.andExpect(status().isAccepted())
+                .andDo(print());
+    }
     @Test
     @Transactional
     @TestDescription("전체 정산 데이터 내역 조회 에러(파라미터 부재)")
     public void getAllCalculations_badRequest_noPathVariable() throws Exception {
+        MockCookie id = new MockCookie(CookieName.ID.getName(), userMember.getId());
+        MockCookie name = new MockCookie(CookieName.NAME.getName(), userMember.getName());
+        MockCookie phoneNum = new MockCookie(CookieName.PHONE_NUM.getName(), userMember.getPhoneNumber());
+        MockCookie isWorker = new MockCookie(CookieName.IS_WORKER.getName(), Integer.toString(userMember.getIsWorker()));
+        MockCookie sessionId = new MockCookie(CookieName.SESSION_ID.getName(), userMember.getSessionId());
+        MockCookie isAdmin = new MockCookie(CookieName.IS_ADMIN.getName(), Integer.toString(userMember.getIsAdmin()));
+        MockCookie isAutoLogin = new MockCookie(CookieName.IS_AUTO_LOGIN.getName(), "false");
+
+        // request
+        ResultActions result = mockMvc.perform(
+                RestDocumentationRequestBuilders.get("/api/calculation/year/{year}/month/{month}/memberSeq",2019,10)
+                        .cookie(id)
+                        .cookie(name)
+                        .cookie(phoneNum)
+                        .cookie(isWorker)
+                        .cookie(sessionId)
+                        .cookie(isAdmin)
+                        .cookie(isAutoLogin)
+        );
+        // result
+        result.andExpect(status().isNotFound())
+                .andDo(print());
     }
 
     @Test
     @Transactional
     @TestDescription("전체 정산 데이터 내역 조회 에러(다른 회원의 내역 조회)")
     public void getAllCalculations_badRequest_requestOtherMemberInfo() throws Exception {
-    }*/
+
+
+        MockCookie id = new MockCookie(CookieName.ID.getName(), userMember.getId());
+        MockCookie name = new MockCookie(CookieName.NAME.getName(), userMember.getName());
+        MockCookie phoneNum = new MockCookie(CookieName.PHONE_NUM.getName(), userMember.getPhoneNumber());
+        MockCookie isWorker = new MockCookie(CookieName.IS_WORKER.getName(), Integer.toString(userMember.getIsWorker()));
+        MockCookie sessionId = new MockCookie(CookieName.SESSION_ID.getName(), userMember.getSessionId());
+        MockCookie isAdmin = new MockCookie(CookieName.IS_ADMIN.getName(), Integer.toString(userMember.getIsAdmin()));
+        MockCookie isAutoLogin = new MockCookie(CookieName.IS_AUTO_LOGIN.getName(), "false");
+
+        // request
+        ResultActions result = mockMvc.perform(
+                RestDocumentationRequestBuilders.get("/api/calculation/year/{year}/month/{month}/memberSeq/{memberSeq}",2019,10,134)
+                        .cookie(id)
+                        .cookie(name)
+                        .cookie(phoneNum)
+                        .cookie(isWorker)
+                        .cookie(sessionId)
+                        .cookie(isAdmin)
+                        .cookie(isAutoLogin)
+        );
+        // result
+        result.andExpect(status().isAccepted())
+                .andDo(print());
+    }
 }
