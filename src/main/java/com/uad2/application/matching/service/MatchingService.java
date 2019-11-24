bba71543a04db385c909ee82f203c3168e7b6a5d
Charles_Lyoo <dkcmsadkcmsa@gmail.com>
2019-11-24 22:46:48 +0900

    [matching]-add update method

--- src/main/java/com/uad2/application/matching/service/MatchingService.java
+++ src/main/java/com/uad2/application/matching/service/MatchingService.java
@@ -4,12 +4,14 @@ package com.uad2.application.matching.service;
  * @DATE 2019-10-20
  */
 
+import com.uad2.application.attendance.dto.AttendanceDto;
 import com.uad2.application.matching.entity.Matching;
 import com.uad2.application.matching.repository.MatchingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class MatchingService {
@@ -26,5 +28,42 @@ public class MatchingService {
 
     public Matching updateMatching(Matching matching) {
         return matchingRepository.save(matching);
+    }
+
+
+    public Matching updateMatchingByAttendance(AttendanceDto.Request request){
+        String availableTime = request.getAvailableTime();//null check
+        String availableDate = request.getAvailableDate();
+        int memberSeq = request.getMemberSeq();
+        if(Objects.isNull(availableTime) || Objects.isNull(availableDate)){
+            throw new RuntimeException("Need null check, availableTime : " + availableTime + ", availableDate : " + availableDate);
+        }
+
+        List<Matching> matchingList = matchingRepository.findByMatchingDate(availableDate);
+        boolean isMatchingExist = Objects.nonNull(matchingList) && matchingList.size() > 0;
+        if(isMatchingExist){
+            for (Matching matching : matchingList) {
+                if(Objects.nonNull(matching)){
+                    //1.해당 매칭의 시간이 요청 시간에 포함 o, 해당 매칭에 요청 memseq 존재 x -> 해당 매칭에 요청 memseq 추가
+                    //2.해당 매칭의 시간이 요청 시간에 포함 x, 해당 매칭에 요청 memseq 존재 o -> 해당 매칭에 요청 memseq 삭제
+                    //3.해당 매칭의 시간이 요청 시간에 포함 o, 해당 매칭에 요청 memseq 존재 o -> 액션x(해당 매칭에 영향 x)
+                    //4.해당 매칭의 시간이 요청 시간에 포함 x, 해당 매칭에 요청 memseq 존재 x -> 액션x(해당 매칭에 영향 x)
+                    if(availableTime.contains(matching.getMatchingTime()) &&
+                            !matching.getAttendMember().contains(Integer.toString(memberSeq))){
+                        matching.setAttendMember(matching.getAttendMember() + "," + memberSeq);
+                        return matching;
+                    }
+                    else if(!availableTime.contains(matching.getMatchingTime()) &&
+                            matching.getAttendMember().contains(Integer.toString(memberSeq))){
+                        System.out.println("delete attendance in matching");
+                        matching.setAttendMember(matching.getAttendMember().replace("," + memberSeq,""));
+                        matching.setAttendMember(matching.getAttendMember().replace("," + memberSeq,""));
+                        matching.setAttendMember(matching.getAttendMember().replace("," + memberSeq,""));
+                        return matching;
+                    }
+                }
+            }
+        }
+        return null;
     }
 }
