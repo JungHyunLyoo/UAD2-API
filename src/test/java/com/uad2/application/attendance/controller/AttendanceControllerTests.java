29b6e2202400942e836528a548dfab0cc5ac7d1b
JungHyunLyoo <dkcmsadkcmsa@gmail.com>
2019-12-06 00:05:54 +0900

    [attendance]-modified test case

--- src/test/java/com/uad2/application/attendance/controller/AttendanceControllerTests.java
+++ src/test/java/com/uad2/application/attendance/controller/AttendanceControllerTests.java
@@ -138,6 +138,27 @@ public class AttendanceControllerTests extends BaseControllerTest {
     }
     @Test
     @Transactional
+    @TestDescription("참가 데이터 생성 오류(파라미터 오류)")
+    public void createAttendance_badRequest_invalidParameter() throws Exception {
+        MockCookie[] userMemberCookieList = super.getUserMemberCookieList(AUTOLOGIN_FALSE);
+        AttendanceDto.Request attendanceRequest = AttendanceDto.Request.builder()
+                .availableDate("2019-11-09")
+                .availableTime("")
+                .memberSeq(userMember.getSeq())
+                .build();
+        // request
+        ResultActions result = mockMvc.perform(
+                super.postRequest("/api/attendance",attendanceRequest,userMemberCookieList)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaTypes.HAL_JSON)
+        );
+        // result
+        result.andExpect(status().isNoContent())
+                .andDo(print());
+
+    }
+    @Test
+    @Transactional
     @TestDescription("참가 데이터 생성 에러(요청 회원 불일치)")
     public void createAttendance_error_notValidMember() throws Exception {
         MockCookie[] userMemberCookieList = super.getUserMemberCookieList(AUTOLOGIN_FALSE);
@@ -196,26 +217,6 @@ public class AttendanceControllerTests extends BaseControllerTest {
     }
     @Test
     @Transactional
-    @TestDescription("참가 데이터 생성 에러(참가 내역 초기화 필요, 그런데 기존 참가내역 존재 x)")
-    public void createAttendance_error_emptyAttendance() throws Exception {
-        MockCookie[] userMemberCookieList = super.getUserMemberCookieList(AUTOLOGIN_FALSE);
-        AttendanceDto.Request attendanceRequest = AttendanceDto.Request.builder()
-                .availableDate("2019-11-02")
-                .availableTime("")
-                .memberSeq(userMember.getSeq())
-                .build();
-        // request
-        ResultActions result = mockMvc.perform(
-                super.postRequest("/api/attendance",attendanceRequest,userMemberCookieList)
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .accept(MediaTypes.HAL_JSON)
-        );
-        // result
-        result.andExpect(status().isAccepted())
-                .andDo(print());
-    }
-    @Test
-    @Transactional
     @TestDescription("참가 데이터 수정 성공")
     public void updateAttendance() throws Exception {
         MockCookie[] userMemberCookieList = super.getUserMemberCookieList(AUTOLOGIN_FALSE);
@@ -236,7 +237,7 @@ public class AttendanceControllerTests extends BaseControllerTest {
     }
     @Test
     @Transactional
-    @TestDescription("참가 데이터 삭제")
+    @TestDescription("참가 데이터 삭제 성공")
     public void deleteAttendance() throws Exception {
         MockCookie[] userMemberCookieList = super.getUserMemberCookieList(AUTOLOGIN_FALSE);
         AttendanceDto.Request attendanceRequest = AttendanceDto.Request.builder()
@@ -252,6 +253,27 @@ public class AttendanceControllerTests extends BaseControllerTest {
         );
         // result
         result.andExpect(status().isNoContent())
+                .andDo(print());
+
+    }
+    @Test
+    @Transactional
+    @TestDescription("참가 데이터 삭제 에러(기존 참가내역 x)")
+    public void deleteAttendance_error_emptyAttendance() throws Exception {
+        MockCookie[] userMemberCookieList = super.getUserMemberCookieList(AUTOLOGIN_FALSE);
+        AttendanceDto.Request attendanceRequest = AttendanceDto.Request.builder()
+                .availableDate("2019-11-02")
+                .availableTime("")
+                .memberSeq(userMember.getSeq())
+                .build();
+        // request
+        ResultActions result = mockMvc.perform(
+                super.postRequest("/api/attendance",attendanceRequest,userMemberCookieList)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaTypes.HAL_JSON)
+        );
+        // result
+        result.andExpect(status().isAccepted())
                 .andDo(print());
     }
 }
